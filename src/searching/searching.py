d508966da08cf5bfd534c55765e78795e4add74c
RobDBennett <robbennett.work@gmail.com>
2020-09-15 12:55:07 -0700

    MVP Day 2, merge sort+ stretch, recursive binary search + stretch.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,15 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if end >= start: #Checks for sorting.
+        mid = start + (end - 1) // 2 #Finds middle.
+        if arr[mid] == target:
+            return mid #If the middles is the target.
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid+1) #If middle is bigger than target, disregard everything to the right of the middle
+        else:
+            return binary_search(arr, target, mid+1, end) #If middle is smaller than target, disregard to left of the middle.
+    else:
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +20,33 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    if arr[0] < arr[1]: #determines if Ascending
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            guess = arr[mid] #The middle of the array is our baseline guess.
+            if target == guess:
+                return mid
+            else:
+                if guess > target: #If the middle is bigger than target, remove everything to the right of middle.
+                    high = mid -1
+                else:
+                    low = mid +1 #If the middle is smaller than the target, remove everything to the left of the middle.
+        return -1
+    else: #For Descending, perform the same tests but largely reverse the values.
+        low = len(arr) - 1
+        high = 0
+        while low >= high:
+            mid = (low + high) // 2
+            guess = arr[mid]
+            if target == guess:
+                return mid
+            else:
+                if guess < target:
+                    low = mid -1
+                    
+                else:
+                    high = mid +1
+        return -1
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,27 +2,89 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
-
+    
+    j = 0 #Starting index for arrA
+    i = 0 #Starting index for arrB
+    k = 0 #Merged index
+    while j < len(arrA) and i < len(arrB):
+        if arrA[j] < arrB[i]: #Compares first element of two arrays
+            merged_arr[k] = arrA[j] #Makes the first element of merged array from A if smaller.
+            j += 1 #Raises the arrA index so will point to next element
+        else:
+            merged_arr[k] = arrB[i] #Makes first element of the merged array from B if smaller
+            i += 1 #Raises the arrB index so will point to the next element.
+        k += 1 #Outside the If, this raises the merged Index.
+    while j < len(arrA): #Continues while the arrA index is less than the length of arrA (only occurs if B goes first)
+        merged_arr[k] = arrA[j] #Adds next element from A to merged.
+        k += 1 #Raises merged index
+        j += 1 #Raises arrA index
+    while i < len(arrB): #Continues while arrB index is less than the length of arrB (only occurs if A goes first)
+        merged_arr[k] = arrB[i] #Adds next element from B to merged
+        k += 1 #Raises merged index
+        i += 1 #Raises arrB index
+        
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
-
-    return arr
+    if len(arr) > 1: #Checks that array is more than single item.
+        right = len(arr) #High
+        left = 0 #Low
+        middle = (right+left) // 2 #Middle.
+        left_merge_arr = merge_sort(arr[:middle]) #Recursively breaks array into two parts 
+        right_merge_arr = merge_sort(arr[middle:]) #Recursively breaks array into two parts.
+        final_arr = merge(left_merge_arr, right_merge_arr) #Runs helper function on array broken into two parts.
+        return final_arr #Returns final product.
+    return arr #Returns if array is single element.
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
-    # Your code here
+    # Start with two halves
+    n1 = mid - start + 1
+    n2 = end - mid
+    # Create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[start + i]
+    for j in range(0, n2):
+        R[j] = arr[mid + 1 + j]
+    # Merge the temp arrays back into arr[l..r]
+    i = 0   #Initial index for first subarray
+    j = 0   #Initial index for second subarray
+    k = start  #Initial index for merged subarray
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    #Copy the remaining elements of L[], if there are any.
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    #Copy the remaining elements of the R[], if there are any.
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
+    # l is the left index, r is the right index.
+    if l < r:
+        #Same as (l+r) //2, but avoids overflow for large l.
+        m = (l+(r-1)) //2
+        #Sort first and second halves.
+        merge_sort_in_place(arr, l, m)
+        merge_sort_in_place(arr, m+1, r)
+        merge_in_place(arr, l, m, r)
 
